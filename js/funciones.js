b15fe6c0dbdd872561c1c4b2ee44c59e171720d9
jaime-navas <116543746+jaime-navas@users.noreply.github.com>
2022-12-19 20:06:22 +0100

    Add files via upload
    Subida de web.

---
+++ js/funciones.js
@@ -0,0 +1,56 @@
+/***************************************************************************************/
+/*
+snipped tomado de:
+https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+Posteriormente modificado
+*/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+/***************************************************************************************/
+
+/***************************************************************************************/
+/*
+snipped tomado de:
+https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+Posteriormente modificado
+*/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+/***************************************************************************************/
